3158ce4c31cac812af3733b1ade5c6df86910602
avadheshchauhan <77477943+avadheshchauhan@users.noreply.github.com>
2021-04-21 16:21:05 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import "./styles.css";
+import { venueDataCollection } from "./venueDB";
+
+let venueKey = Object.keys(venueDataCollection);
+
+export default function App() {
+  const [selectGenre, setGenre] = useState("Adventure");
+  function genreClickHandler(genre) {
+    setGenre(genre);
+  }
+  return (
+    <div className="App">
+      <h1 style={{ textAlign: "left" }}>
+        {" "}
+        <span>🏕</span>️<span>🏛</span>️ Travel Diaries <span>🏛</span>️
+        <span>🏕</span>️
+      </h1>
+      <p style={{ fontSize: "smaller", textAlign: "left" }}>
+        {" "}
+        👇👇 Checkout some popular venues in India. Select a type to get started
+        👇👇
+      </p>
+
+      <div style={{ textAlign: "left" }}>
+        {venueKey.map((genre) => (
+          <button
+            key={genre}
+            onClick={() => genreClickHandler(genre)}
+            style={{
+              cursor: "pointer",
+              background: "grey",
+              border: "1px solid black",
+              borderRadius: "0.5rem",
+              padding: "0.5rem 0.5rem",
+              margin: "1rem 0.3rem",
+              fontWeight: "bold",
+              color: "wheat",
+              outline: "none"
+            }}
+          >
+            {genre}
+          </button>
+        ))}
+      </div>
+      <hr />
+      <div style={{ textAlign: "left" }}>
+        <ul style={{ paddingInlineStart: "0" }}>
+          {venueDataCollection[selectGenre].map((parameter) => (
+            <li
+              key={parameter}
+              style={{
+                listStyle: "none",
+                padding: "1rem",
+                border: "1px solid #D1D2DB",
+                width: "70%",
+                margin: "1rem 0rem",
+                borderRadius: "0.8rem"
+              }}
+            >
+              <div style={{ fontSize: "larger" }}>{parameter.place}</div>
+              <hr />
+              <div style={{ fontSize: "smaller" }}>{parameter.famous}</div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
---
+++ src/venueDB.js
@@ -0,0 +1,136 @@
+export const venueDataCollection = {
+  Adventure: [
+    {
+      place: "Ladakh",
+      famous:
+        "CHADAR TREK - The Chadar Trek or the Zanskar Gorge is a winter trail over the frozen Zanskar River, which lies in the Indian union territory of Ladakh. It is traditionally the only means of travel in the area during the harsh winter months. The trail has become popular with international adventure tourists."
+    },
+    {
+      place: "Rishikesh",
+      famous:
+        "WHITE RIVER RAFTING - The 25 KM long route is famed for offering more than 12 rapids of grade three and above. Depending on the various grades of rapids, the charges of Rishikesh Rafting can range from INR 550 to INR 3000 per person. The best time for white water rafting in Rishikesh is post-monsoon and early summers."
+    },
+    {
+      place: "Jaipur",
+      famous:
+        "HOT AIR BALLOONING - Fly high in the sky in a giant vibrant coloured hot air balloon and capture the view of the whole city. ... The excitement of flying over palaces, forts, and lakes during the balloon safari in Jaipur is quite special."
+    },
+    {
+      place: "Mysore",
+      famous:
+        "SKYDIVING - It has two types of jumps. One is Tandem Jump and the other is Freefall. Both the jumps are done from an altitude of 10000ft. The free Fall is a solo experience where you jump from anywhere between 10000 ft to 15000 ft."
+    },
+    {
+      place: "Satpura",
+      famous:
+        "ROCK CLIMBING - It is thrilling adventure sports in Satpura. Anchoring and rappelling become an addiction in these sedimentary rocks which have gems like little pebbles engraved in them providing you finger holds."
+    }
+  ],
+  Beach: [
+    {
+      place: "Goa",
+      famous:
+        "SURFING - Surfing is a surface water sport in which an individual, a surfer, uses a board to ride on the forward section, or face, of a moving wave of water, which usually carries the surfer towards the shore."
+    },
+    {
+      place: "Andaman",
+      famous:
+        "SCUBA DIVING - Underwater diving, as a human activity, is the practice of descending below the water's surface to interact with the environment. Immersion in water and exposure to high ambient pressure have physiological effects that limit the depths and duration possible in ambient pressure diving."
+    },
+    {
+      place: "Gokarna",
+      famous:
+        "BANANA BOAT RIDE - A banana boat (or water sled), is an unpowered, inflatable recreational boat meant to be towed. Different models usually accommodate three to ten riders sitting on a larger, main tube and resting their feet on two laterally flanking tubes which stabilize the boat. The main tube is often yellow and banana-shaped."
+    },
+    {
+      place: "Bekal",
+      famous:
+        "KITE SAILING - Kiteboarding or kitesurfing is an extreme sport where the kiter uses the wind power with a large power kite to be pulled on a water, land or snow surface. It combines aspects of paragliding, surfing, windsurfing, skateboarding, snowboarding and wakeboarding."
+    },
+    {
+      place: "Minicoy",
+      famous:
+        "WATER SKIING - Water skiing is a surface water sport in which an individual is pulled behind a boat or a cable ski installation over a body of water, skimming the surface on two skis or one ski."
+    }
+  ],
+  Historical: [
+    {
+      place: "Jaipur",
+      famous:
+        "HAWA MAHAL - Hawa Mahal (English translation: The Palace of Winds or The Palace of Breeze) is a palace in Jaipur, India approximately 300 kilometers from the capital city of Delhi. Built from red and pink sandstone, the palace sits on the edge of the City Palace, Jaipur, and extends to the Zenana, or women's chambers."
+    },
+    {
+      place: "Kolkata",
+      famous:
+        "VICTORIA MEMORIAL - The Victoria Memorial is a large marble building in Kolkata, West Bengal, India, which was built between 1906 and 1921. It is dedicated to the memory of Empress Victoria, and is now a museum under the auspices of the Ministry of Culture. The memorial lies on the Maidan."
+    },
+    {
+      place: "Jodhpur",
+      famous:
+        "MEHRANGARH FORT - It is one of the largest forts in India. Built around 1459 by Rao Jodha, the fort is situated 410 feet above the city and is enclosed by imposing thick walls. Inside its boundaries there are several palaces known for their intricate carvings and expansive courtyards"
+    },
+    {
+      place: "Aurangabad",
+      famous:
+        "AJANTA AND ELLORA CAVES - It is one of the largest rock-cut Hindu temple cave complexes in the world, featuring Hinduism in particular and few Buddhist and Jain monuments with Artwork dating from the 600–1000 CE period."
+    },
+    {
+      place: "Delhi",
+      famous:
+        "RED FORT - The Red Fort is a historic fort in the city of Delhi (in Old Delhi) in India that served as the main residence of the Mughal Emperors. Emperor Shah Jahan commissioned construction of the Red Fort on 12 May 1638, when he decided to shift his capital from Agra to Delhi. Originally red and white, its painting is credited to architect Ustad Ahmad Lahori, who also constructed the Taj Mahal. It was renovated between May 1639 and April 1648 based on an earlier fort."
+    }
+  ],
+  Snow: [
+    {
+      place: "Gulmarg",
+      famous:
+        "SKIING - It is a means of transport using skis to glide on snow. Variations of purpose include basic transport, a recreational activity, or a competitive winter sport. Many types of competitive skiing events are recognized by the International Olympic Committee, and the International Ski Federation."
+    },
+    {
+      place: "Rohtang",
+      famous:
+        "Sledding - It is a winter sport typically carried out in a prone or seated position on a vehicle generically known as a sled, a sledge, or a sleigh. It is the basis of three Olympic sports: luge, skeleton and bobsledding. When practised on sand, it is known as a form of sandboarding"
+    },
+    {
+      place: "Auli",
+      famous:
+        "SNOWBOARDING - It is a recreational and competitive activity that involves descending a snow-covered slope while standing on a snowboard that is almost always attached to a rider's feet. It features in the Winter Olympic Games and Winter Paralympic Games."
+    },
+    {
+      place: "Kedarkantha",
+      famous: "Trekking"
+    },
+    {
+      place: "Manali",
+      famous:
+        "ICE CLIMBING - Ice climbing is the activity of ascending inclined ice formations. Usually, ice climbing refers to roped and protected climbing of features such as icefalls, frozen waterfalls, and cliffs and rock slabs covered with ice refrozen from flows of water."
+    }
+  ],
+  Farming: [
+    {
+      place: "Jammu",
+      famous:
+        "APPLE - Farmers in Jammu have developed an apple tree variety that can be grown in temperatures ranging between 40-45 degree celsius. Called 'Hariman 99 Summer Zone Apple,' farmers in Jammu's Samba have successfully grown the apple orchard, a feet people thought unthinkable."
+    },
+    {
+      place: "Chikmagalur",
+      famous:
+        "COFFEE - Chikmagalur is a paradise in itself. Located in the foothills of the Mullayanagiri range, the city is known for its beautiful climate and coffee plantations. Due to these coffee plantations, Chikmagalur is also known as “The Land of coffee”."
+    },
+    {
+      place: "Darjeeling",
+      famous:
+        "TEA - Darjeeling tea, often called the fine wine of teas, is famous for its special aroma and is preferred by connoisseurs around the world. ... The tea produced in the region is processed and manufactured in factories in Darjeeling before it is sent to other parts of India and exported around the world."
+    },
+    {
+      place: "Nagpur",
+      famous:
+        "ORANGE - Nagpur known as the Orange City for being a major trade center of oranges cultivated in large part of the region. ... The British East India Company took over Nagpur in the 19th century and made it the capital of the Central Provinces and Berar."
+    },
+    {
+      place: "Jalgaon",
+      famous:
+        "BANANA - Jalgaon district in North Maharashtra is a hub of banana production. It accounts for over 50% of the state's 90,000 hectares under banana plantation. Over 75 tons of of the fruit are produced per hectare here."
+    }
+  ]
+};
